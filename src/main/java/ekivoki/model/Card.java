876b41c46621739bfe6207f7d25ebda0709b4b8e
Vyacheslav Manzhela <slavamanzh@gmail.com>
2022-03-27 22:24:31 +0300

    feat: add catd, game and session files

---
+++ src/main/java/ekivoki/model/Card.java
@@ -0,0 +1,16 @@
+package ekivoki.model;
+
+import java.util.Date;
+
+public class Card {
+
+    private long id;
+    private long topicId;
+    private long questionId;
+    private int questionNumber;
+    private String questionText;
+    private int leadTime;
+    private Date dateCreation;
+    private Date lastModified;
+    private Long version;
+}
---
+++ src/main/java/ekivoki/model/Game.java
@@ -0,0 +1,8 @@
+package ekivoki.model;
+
+public class Game {
+
+    private long id;
+    private long sessionId;
+    private long cardId;
+}
