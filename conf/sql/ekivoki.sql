c06d2a65031f62c49f233c009ef2068737ae2fb3
Vyacheslav Manzhela <slavamanzh@gmail.com>
2022-03-29 16:21:41 +0300

    docs: optimized parameter types in sql file

--- conf/sql/ekivoki.sql
+++ conf/sql/ekivoki.sql
@@ -11,38 +11,39 @@ USE `ekivoki`;
 
 DROP TABLE IF EXISTS `ek_card`;
 CREATE TABLE `ek_card` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `topic_id` bigint(20) NOT NULL,
-  `question_id` bigint(20) NOT NULL,
-  `question_number` int(6) NOT NULL,
+  `question_id` bigint(20) unsigned NOT NULL,
+  `question_number` tinyint(1) NOT NULL,
   `question_text` varchar(50) NOT NULL,
-  `lead_time` int(20) NOT NULL,
+  `lead_time` tinyint(1) NOT NULL,
   `date_creation` datetime NOT NULL,
   `last_modified` datetime NOT NULL,
-  `version` int(20) NOT NULL,
+  `version` tinyint(2) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `question_id` (`question_id`),
   KEY `topic_id` (`topic_id`),
+  KEY `question_id` (`question_id`),
   CONSTRAINT `ek_card_ibfk_1` FOREIGN KEY (`topic_id`) REFERENCES `ek_topic` (`id`),
-  CONSTRAINT `ek_card_ibfk_2` FOREIGN KEY (`question_id`) REFERENCES `ek_question` (`id`),
-  CONSTRAINT `ek_card_ibfk_3` FOREIGN KEY (`topic_id`) REFERENCES `ek_topic` (`id`)
+  CONSTRAINT `ek_card_ibfk_2` FOREIGN KEY (`question_id`) REFERENCES `ek_question` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
 DROP TABLE IF EXISTS `ek_game`;
 CREATE TABLE `ek_game` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `session_id` bigint(20) NOT NULL,
-  `card_id` bigint(20) NOT NULL,
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `session_id` bigint(20) unsigned NOT NULL,
+  `card_id` bigint(20) unsigned NOT NULL,
   PRIMARY KEY (`id`),
   KEY `session_id` (`session_id`),
-  CONSTRAINT `ek_game_ibfk_1` FOREIGN KEY (`session_id`) REFERENCES `ek_session` (`id`)
+  KEY `card_id` (`card_id`),
+  CONSTRAINT `ek_game_ibfk_1` FOREIGN KEY (`session_id`) REFERENCES `ek_session` (`id`),
+  CONSTRAINT `ek_game_ibfk_2` FOREIGN KEY (`card_id`) REFERENCES `ek_card` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
 DROP TABLE IF EXISTS `ek_question`;
 CREATE TABLE `ek_question` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   `description` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
@@ -51,7 +52,7 @@ CREATE TABLE `ek_question` (
 
 DROP TABLE IF EXISTS `ek_session`;
 CREATE TABLE `ek_session` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `session_uuid` varchar(36) NOT NULL,
   `date_creation` datetime NOT NULL,
   PRIMARY KEY (`id`)
@@ -66,4 +67,4 @@ CREATE TABLE `ek_topic` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
--- 2022-03-27 19:43:00
+-- 2022-03-29 13:19:02
